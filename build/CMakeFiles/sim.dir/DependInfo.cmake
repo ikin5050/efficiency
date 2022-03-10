a4fa5fd909ba41c41fb54103584e5642d1a34624
ikin5050 <60651617+ikin5050@users.noreply.github.com>
2022-03-10 19:16:24 +0000

    Add files via upload

---
+++ build/CMakeFiles/sim.dir/DependInfo.cmake
@@ -0,0 +1,59 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/paulw/efficiency/action.cc" "/home/paulw/efficiency/build/CMakeFiles/sim.dir/action.cc.o"
+  "/home/paulw/efficiency/construction.cc" "/home/paulw/efficiency/build/CMakeFiles/sim.dir/construction.cc.o"
+  "/home/paulw/efficiency/detector.cc" "/home/paulw/efficiency/build/CMakeFiles/sim.dir/detector.cc.o"
+  "/home/paulw/efficiency/event.cc" "/home/paulw/efficiency/build/CMakeFiles/sim.dir/event.cc.o"
+  "/home/paulw/efficiency/generator.cc" "/home/paulw/efficiency/build/CMakeFiles/sim.dir/generator.cc.o"
+  "/home/paulw/efficiency/physics.cc" "/home/paulw/efficiency/build/CMakeFiles/sim.dir/physics.cc.o"
+  "/home/paulw/efficiency/run.cc" "/home/paulw/efficiency/build/CMakeFiles/sim.dir/run.cc.o"
+  "/home/paulw/efficiency/sim.cc" "/home/paulw/efficiency/build/CMakeFiles/sim.dir/sim.cc.o"
+  "/home/paulw/efficiency/stepping.cc" "/home/paulw/efficiency/build/CMakeFiles/sim.dir/stepping.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4UI_USE_XM"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_OPENGLXM"
+  "G4VIS_USE_RAYTRACERX"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/cern/geant4.10.07.p02/install/include/Geant4"
+  "/mnt/share/cern_CentOS7/geant4.10.07.p02/install/include/Geant4"
+  "/usr/include/qt5"
+  "/usr/include/qt5/QtCore"
+  "/usr/lib64/qt5/./mkspecs/linux-g++"
+  "/usr/include/qt5/QtGui"
+  "/usr/include/qt5/QtWidgets"
+  "/usr/include/qt5/QtOpenGL"
+  "/usr/include/qt5/QtPrintSupport"
+  "/cern/xerces-c-3.1.4/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
